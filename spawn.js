62b750aefc7126484b10622f3ba8dfb2632520d1
Vince Elizaga <vncelizaga@gmail.com>
2024-06-16 12:00:43 -0400

    update battery provider

--- spawn.js
+++ spawn.js
@@ -1,23 +1,20 @@
 const { spawn } = require('child_process');
 
-async function executeCommand(command, args) {
+async function executeCommand(command) {
   return new Promise((resolve, reject) => {
-    const childProcess = spawn(command, args);
+    const childProcess = spawn(command, {
+      shell: true,
+      stdio: 'inherit'
+    });
     let stdoutData = '';
 
     // Listen for stdout data
-    childProcess.stdout.on('data', (data) => {
+    childProcess.on('data', (data) => {
       stdoutData += data.toString();
-    });
-
-    // Listen for stderr data
-    childProcess.stderr.on('data', (data) => {
-      console.error(`stderr: ${data}`);
     });
 
     // Listen for the process to exit
     childProcess.on('close', (code) => {
-      console.log(`child process exited with code ${code}`);
       resolve(stdoutData);
     });
 
@@ -30,21 +27,24 @@ async function executeCommand(command, args) {
 }
 
 const activateKVS = () => {
-    setTimeout(()=>{
-        const kvsClient = spawn('/home/pi/kvs/kvsWebrtcClientMasterGstSample', [process.env.DEVICE_ID]);    
-    }, 5000);
+  setTimeout(()=>{
+      const kvsClient = spawn('/home/pi/kvs/kvsWebrtcClientMasterGstSample', [process.env.DEVICE_ID]);    
+  }, 5000);
 }
 
 const getBatteryLevel = async () =>{
-    return await executeCommand('echo "get battery" | nc -q 0 127.0.0.1 8423');
+  const result = await executeCommand('echo "get battery" | nc -q 0 127.0.0.1 8423');
+  return parseFloat(result?.split(' ')[1] || 0);
 }
 
 const getBatteryCharging = async () =>{
-    return await executeCommand('echo "get battery_charging" | nc -q 0 127.0.0.1 8423');
+  const result = await executeCommand('echo "get battery_charging" | nc -q 0 127.0.0.1 8423');
+  return result?.split(' ')[1] === 'true' ? true : false; 
 }
 
 const getBatteryVoltage = async () =>{
-    return await executeCommand('echo "get battery_v" | nc -q 0 127.0.0.1 8423');
+  const result = await executeCommand('echo "get battery_v" | nc -q 0 127.0.0.1 8423');
+  return parseFloat(result?.split(' ')[1] || 0); 
 }
 
 module.exports = {
